1c33d57c16c454f31c813b79bc16b9a87386c90c
JoonHyukYoon <crayngnuk1@naver.com>
2024-01-28 17:14:45 +0900

    feat: MyPageController 마이페이지 메인 화면 api 생성

---
+++ src/main/java/com/ll/traveler/domain/member/myPage/controller/MyPageController.java
@@ -0,0 +1,27 @@
+package com.ll.traveler.domain.member.myPage.controller;
+
+import com.ll.traveler.domain.member.member.entity.Member;
+import com.ll.traveler.domain.member.myPage.service.MyPageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/my-page")
+@RequiredArgsConstructor // 생성자 주입
+public class MyPageController {
+    private final MyPageService myPageService;
+
+    @GetMapping("/{id}")
+    public String myPageMain(@PathVariable("id") String id, Model model) {
+        Member member = myPageService.getMemberInfo(id);
+
+        // 회원정보
+        model.addAttribute("member", member);
+
+        return "domain/member/myPage/main";
+    }
+}
