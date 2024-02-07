f6afc319dbad12ddf457d4342402bca94e570240
caaano <wlsrl0515@naver.com>
2024-02-07 13:17:22 +0900

    fix: detail page refactoring

--- src/main/java/com/ll/traveler/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/traveler/domain/post/post/controller/PostController.java
@@ -7,9 +7,6 @@ import com.ll.traveler.global.rq.Rq;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-
-import com.ll.traveler.global.rq.Rq;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
@@ -20,12 +17,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 import java.util.ArrayList;
 import java.util.List;
