32cd0695e4583b938d8ce6be49c44c8c310f68bc
JoonHyukYoon <crayngnuk1@naver.com>
2024-02-13 17:42:20 +0900

    feat: 노트북 오류안나는 코드

--- src/main/java/com/ll/traveler/domain/member/myPage/dto/PasswordForm.java
+++ src/main/java/com/ll/traveler/domain/member/myPage/dto/PasswordForm.java
@@ -1,19 +1,13 @@
-package com.ll.traveler.domain.member.myPage.dto0;
+package com.ll.traveler.domain.member.myPage.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-
-@Data
+@Getter
+@Setter
 public class PasswordForm {
     private Long memberId;
-
-    @NotBlank(message = "현재 비밀번호를 입력해주세요.")
     private String currentPassword;
-
-    @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
     private String newPassword;
-
-    @NotBlank(message = "새로운 비밀번호 확인을 입력해주세요.")
     private String confirmPassword;
 }
