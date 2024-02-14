6805f8a60f5f427b4439fd73ebc893509cffe5f6
leedahye426 <9535957@naver.com>
2024-02-14 15:13:13 +0900

    add: user-myPost

--- src/main/java/com/ll/traveler/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/ll/traveler/domain/post/post/repository/PostRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByTitleContaining(String kw, Pageable pageable);
     Page<Post> findByAreaContaining(String kw, Pageable pageable);
     Page<Post> findByCategoriesContentContaining(String kw, Pageable pageable);
     Page<Post> findByAuthorNicknameContaining(String kw, Pageable pageable);
-
+    List<Post> findByAuthorId(Long id);
 }
--- src/main/java/com/ll/traveler/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/traveler/domain/post/post/service/PostService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -54,5 +55,9 @@ public class PostService {
 
     public Optional<Post> findById(long id) {
         return postRepository.findById(id);
+    }
+
+    public List<Post> findByAuthorId(Long id) {
+        return postRepository.findByAuthorId(id);
     }
 }
