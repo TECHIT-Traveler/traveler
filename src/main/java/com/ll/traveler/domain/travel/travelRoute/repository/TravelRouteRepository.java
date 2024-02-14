4b1ed43cf754504524110b4d5a68167178586959
leedahye426 <9535957@naver.com>
2024-02-14 11:35:22 +0900

    add: travel route list

--- src/main/java/com/ll/traveler/domain/travel/travelRoute/repository/TravelRouteRepository.java
+++ src/main/java/com/ll/traveler/domain/travel/travelRoute/repository/TravelRouteRepository.java
@@ -2,6 +2,7 @@ package com.ll.traveler.domain.travel.travelRoute.repository;
 
 import com.ll.traveler.domain.travel.travelRoute.entity.TravelRoute;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TravelRouteRepository extends JpaRepository<TravelRoute, Long> {
