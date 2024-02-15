89a739ae6ad3c9441c2e81888262806772d8e69c
JoonHyukYoon <crayngnuk1@naver.com>
2024-02-15 14:32:46 +0900

    2월15일 오후2시30분 내가 쓴 댓글 구현 완료

--- src/main/java/com/ll/traveler/domain/member/myPage/controller/MyPageController.java
+++ src/main/java/com/ll/traveler/domain/member/myPage/controller/MyPageController.java
@@ -3,6 +3,7 @@ package com.ll.traveler.domain.member.myPage.controller;
 import com.ll.traveler.domain.member.member.entity.Member;
 import com.ll.traveler.domain.member.myPage.dto.MyPageUpdateForm;
 import com.ll.traveler.domain.member.myPage.service.MyPageService;
+import com.ll.traveler.domain.post.postComment.entity.PostComment;
 import com.ll.traveler.global.rq.Rq;
 import com.ll.traveler.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
 @Controller
 @RequestMapping("/my-page")
-@RequiredArgsConstructor // 생성자 주입
+@RequiredArgsConstructor
 public class MyPageController {
     private final MyPageService myPageService;
     private final Rq rq;
@@ -20,9 +24,9 @@ public class MyPageController {
     @GetMapping("/{id}")
     public String myPageMain(@PathVariable("id") Long id, Model model) {
         Member member = myPageService.getMemberInfo(id);
-
-        // 회원정보
         model.addAttribute("member", member);
+        List<PostComment> comments = myPageService.getMyComments(id);
+        model.addAttribute("comments", comments);
 
         return "domain/member/myPage/main";
     }
@@ -30,10 +34,7 @@ public class MyPageController {
     @GetMapping("/{id}/update")
     public String memberInfoUpdateMain(@PathVariable("id") Long id, Model model) {
         Member member = myPageService.getMemberInfo(id);
-
-        // 회원정보
         model.addAttribute("member", member);
-
         return "domain/member/myPage/update";
     }
 
@@ -42,10 +43,17 @@ public class MyPageController {
             , MyPageUpdateForm form
             , Model model) {
         RsData<Void> memberRs = myPageService.updateMemberInfo(id, form.getEmail(), form.getNickName());
-
         Member member = myPageService.getMemberInfo(id);
         model.addAttribute("member", member);
-
         return rq.redirectOrBack(memberRs, "/my-page/" + id);
     }
+
+//
+//    @GetMapping("/{id}/comments")
+//    public String getMyComments(@PathVariable("id") Long id, Model model) {
+//        List<PostComment> comments = myPageService.getMyComments(id);
+//        model.addAttribute("comments", comments);
+//        return "my<Comments>";
+//    }
+
 }
--- src/main/java/com/ll/traveler/domain/post/post/repository/PostCommentRepository.java
+++ src/main/java/com/ll/traveler/domain/post/post/repository/PostCommentRepository.java
@@ -3,9 +3,12 @@ package com.ll.traveler.domain.post.post.repository;
 
 import com.ll.traveler.domain.post.postComment.entity.PostComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
     Optional<PostComment> findCommentById(long id);
+    List<PostComment> findByAuthorId(Long memberId);
 }
